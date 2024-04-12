5c2dd016215356c6492fc3ba12cc672418ca7f6c
Rohan Jadvani <rohan@hey.com>
2024-04-12 14:09:36 -0400

    WIP

--- src/dkg/round3.rs
+++ src/dkg/round3.rs
@@ -43,43 +43,75 @@ where
         round1_public_packages.iter().map(|pkg| pkg.identity()),
     );
 
+    let mut identities = BTreeMap::new();
     let mut round1_frost_packages: BTreeMap<Identifier, Round1Package> = BTreeMap::new();
     for public_package in round1_public_packages.clone() {
         if public_package.checksum() != expected_round1_checksum {
             return Err(Error::ChecksumError(ChecksumError::DkgPublicPackageError));
         }
 
-        if public_package.identity() == identity {
-            continue;
+        let identity = public_package.identity();
+        let frost_identifier = identity.to_frost_identifier();
+        let frost_package = public_package.frost_package().clone();
+
+        if round1_frost_packages
+            .insert(frost_identifier, frost_package)
+            .is_some()
+        {
+            return Err(Error::InvalidInput(format!(
+                "multiple public packages provided for identity {}",
+                public_package.identity()
+            )));
         }
 
+        identities.insert(frost_identifier, identity);
         round1_frost_packages.insert(
             public_package.identity().to_frost_identifier(),
             public_package.frost_package().clone(),
         );
     }
 
-    let round2_public_packages = round2_public_packages.into_iter().collect::<Vec<_>>();
+    assert_eq!(round1_public_packages.len(), identities.len());
+    assert_eq!(round1_public_packages.len(), round1_frost_packages.len());
 
+    // The public package for `identity` must be excluded from `frost::keys::dkg::part3`
+    // inputs
+    round1_frost_packages
+        .remove(&identity.to_frost_identifier())
+        .expect("missing public package for identity");
+
+    let round2_public_packages = round2_public_packages.into_iter().collect::<Vec<_>>();
     let expected_round2_checksum = round2::input_checksum(
         round1_public_packages.iter().map(Borrow::borrow),
     );
 
     let mut round2_frost_packages: BTreeMap<Identifier, Round2Package> = BTreeMap::new();
-    for package in round2_public_packages {
-        if package.checksum() != expected_round2_checksum {
+    for public_package in round2_public_packages {
+        if public_package.checksum() != expected_round2_checksum {
             return Err(Error::ChecksumError(ChecksumError::DkgPublicPackageError));
         }
 
-        if package.identity() == identity {
-            continue;
+        let identity = public_package.identity();
+        let frost_identifier = identity.to_frost_identifier();
+        let frost_package = public_package.frost_package().clone();
+
+        if round2_frost_packages
+            .insert(frost_identifier, frost_package)
+            .is_some()
+        {
+            return Err(Error::InvalidInput(format!(
+                "multiple public packages provided for identity {}",
+                public_package.identity()
+            )));
         }
 
         round2_frost_packages.insert(
-            package.identity().to_frost_identifier(),
-            package.frost_package().clone(),
+            public_package.identity().to_frost_identifier(),
+            public_package.frost_package().clone(),
         );
     }
+
+    assert_eq!(round1_frost_packages.len(), round2_frost_packages.len());
 
     let (key_package, public_key_package) = part3(
         round2_secret_package,
@@ -102,12 +134,14 @@ mod tests {
 
     #[test]
     fn test_round3() {
-        let secret = Secret::random(thread_rng());
-        let identity1 = secret.to_identity();
-        let identity2 = Secret::random(thread_rng()).to_identity();
-        let identity3 = Secret::random(thread_rng()).to_identity();
+        let secret1 = Secret::random(thread_rng());
+        let secret2 = Secret::random(thread_rng());
+        let secret3 = Secret::random(thread_rng());
+        let identity1 = secret1.to_identity();
+        let identity2 = secret2.to_identity();
+        let identity3 = secret3.to_identity();
 
-        let (round1_secret_package, package1) = round1::round1(
+        let (round1_secret_package_1, package1) = round1::round1(
             &identity1,
             2,
             [&identity1, &identity2, &identity3],
@@ -115,7 +149,7 @@ mod tests {
         )
         .expect("round 1 failed");
 
-        let (_, package2) = round1::round1(
+        let (round1_secret_package_2, package2) = round1::round1(
             &identity2,
             2,
             [&identity1, &identity2, &identity3],
@@ -123,7 +157,7 @@ mod tests {
         )
         .expect("round 1 failed");
 
-        let (_, package3) = round1::round1(
+        let (round1_secret_package_3, package3) = round1::round1(
             &identity3,
             2,
             [&identity1, &identity2, &identity3],
@@ -131,25 +165,49 @@ mod tests {
         )
         .expect("round 1 failed");
 
-        let round1_secret_package = round1::import_secret_package(&round1_secret_package, &secret)
+        let round1_secret_package_1 = round1::import_secret_package(&round1_secret_package_1, &secret1)
             .expect("secret package import failed");
-
-        let (encrypted_secret_package, round2_public_packages) = round2::round2(
+        let (encrypted_secret_package, _) = round2::round2(
             &identity1,
-            &round1_secret_package,
+            &round1_secret_package_1,
             [&package1, &package2, &package3],
             thread_rng(),
         )
         .expect("round 2 failed");
 
-        let secret_package = import_secret_package(&encrypted_secret_package, &secret)
+        let round1_secret_package_2 = round1::import_secret_package(&round1_secret_package_2, &secret2)
+            .expect("secret package import failed");
+        let (_, round2_public_packages_2) = round2::round2(
+            &identity2,
+            &round1_secret_package_2,
+            [&package1, &package2, &package3],
+            thread_rng(),
+        )
+        .expect("round 2 failed");
+
+        let round1_secret_package_3 = round1::import_secret_package(&round1_secret_package_3, &secret3)
+            .expect("secret package import failed");
+        let (_, round2_public_packages_3) = round2::round2(
+            &identity3,
+            &round1_secret_package_3,
+            [&package1, &package2, &package3],
+            thread_rng(),
+        )
+        .expect("round 2 failed");
+
+        let round2_public_packages = [
+            round2_public_packages_2.get(&identity1).expect("should have package for identity1"),
+            round2_public_packages_3.get(&identity1).expect("should have package for identity1"),
+        ];
+
+        let secret_package = import_secret_package(&encrypted_secret_package, &secret1)
             .expect("round 2 secret package import failed");
 
         let (key_package, public_key_package) = round3(
             &identity1,
             &secret_package,
             [&package1, &package2, &package3],
-            round2_public_packages.values(),
+            round2_public_packages,
         )
         .expect("round 3 failed");
     }
