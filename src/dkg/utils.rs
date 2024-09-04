125c6e8f61412f778aba494927e3d0e1a495580c
neithanmo <neithanmo@gmail.com>
2024-09-04 16:11:46 -0600

    feature out sdk logging functions in tests

--- src/dkg/utils.rs
+++ src/dkg/utils.rs
@@ -1,23 +1,30 @@
-
-extern "C"{
+#[cfg(not(test))]
+extern "C" {
     fn check_app_canary();
     fn zemu_log_stack(ctx: *const u8);
     fn zemu_log(buf: *const u8);
 }
 
-pub fn z_check_app_canary(){
-    unsafe{
+pub fn z_check_app_canary() {
+    #[cfg(not(test))]
+    unsafe {
         check_app_canary()
     }
 }
 
-pub fn zlog(buf: &str){
-    unsafe{
+pub fn zlog(buf: &str) {
+    #[cfg(not(test))]
+    unsafe {
         zemu_log(buf.as_bytes().as_ptr())
     }
+    #[cfg(test)]
+    std::println!("{}", buf);
 }
-pub fn zlog_stack(buf: &str){
-    unsafe{
+pub fn zlog_stack(buf: &str) {
+    #[cfg(not(test))]
+    unsafe {
         zemu_log_stack(buf.as_bytes().as_ptr())
     }
+    #[cfg(test)]
+    std::println!("{}", buf);
 }
